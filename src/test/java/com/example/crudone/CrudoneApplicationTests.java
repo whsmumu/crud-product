8c6ccc95049f6399c60e47096b075f2ef5953690
mumu <123791887+whsmumu@users.noreply.github.com.>
2025-03-10 01:10:10 -0300

    feat: create flyway sql

---
+++ src/test/java/com/example/crudone/CrudoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
